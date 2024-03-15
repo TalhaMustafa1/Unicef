8c523558f03c25f37a4d19cacdc2469cf8284ccd
Talha Mustafa <91594421+TalhaMustafa1@users.noreply.github.com>
2024-03-15 22:33:21 +0500

    js to its own file

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function changePlayer(source, name, description,subdescription) {
+    document.getElementById('audio-player').src = source + '.mp3';
+    document.getElementById('player-name').textContent = name;
+    document.getElementById('player-description').textContent = description;
+    document.getElementById('subdescription').textContent=subdescription;
+}
+
+function toggleDescription() {
+    var description = document.querySelector('.description');
+    var toggleButton = document.getElementById('toggle-description');
+    var hideDescription=document.getElementById('hide-description');
+    if (description.style.display === 'none' || description.style.display === '') {
+        description.style.display = 'block';
+        toggleButton.style.display = 'none';
+        hideDescription.style.display='block'; // Hide the toggle button
+    }
+}
+
+function hideDescription() {
+    var description = document.querySelector('.description');
+    var toggleButton = document.getElementById('toggle-description');
+    var hideDescription=document.getElementById('hide-description');
+    description.style.display = 'none';
+    hideDescription.style.display='none';
+    toggleButton.style.display = 'block'; // Show the toggle button
+}
